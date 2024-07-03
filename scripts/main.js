3b6a4d116aa64f833d2ec242bf947a6f41b6dc77
maidchannel <joseph.kline@ymail.com>
2024-07-03 13:41:43 -0500

    Update main.js
    add event upon clicking

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,6 @@ const myHeading = document.querySelector("h1");
 myHeading.textContent = "Maid Channel";
 
 // Creates event when clicking image (pop-up/prompt)
-//document.querySelector("html").addEventListener("click", () => {
-//    alert("Help, I can't draw maids!");
-//  });
+document.querySelector("html").addEventListener("click", () => {
+    alert("Help, I can't draw maids!");
+  });
