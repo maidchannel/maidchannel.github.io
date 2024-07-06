fa480e25a1c054695b2aa9771fb2def98ff23696
maidchannel <joseph.kline@ymail.com>
2024-07-06 09:41:19 -0500

    v0.1 update
    adjusted CSS, added non-functional button for future use

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,22 @@
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Maid Channel";
 
-// Creates event when clicking image (pop-up/prompt)
-document.querySelector("html").addEventListener("click", () => {
-    alert("Help, I can't draw maids!");
-  });
+// Creates event when clicking anything (pop-up/prompt)
+//document.querySelector("html").addEventListener("click", () => {
+ //   alert("Help, I can't draw maids!");
+//  });
+
+  // Adds a button to click and returns response in text
+  document.addEventListener("DOMContentLoaded", () => {
+    function createParagraph() {
+        const para = document.createElement("p");
+        para.textContent = "This button reserved for future use, don't touch me!"
+        document.body.appendChild(para);
+    }
+  })
+
+  const buttons = document.querySelectorAll("button");
+
+  for (const button of buttons) {
+    button.addEventListener("click", createParagraph)
+  }
